52f4c0b23159032185f4bcd15fb2a28c206b3224
Andrew Chan <AndrewTziAnChan@gmail.com>
2024-08-25 19:55:35 -0400

    Initial commit

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1632676.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 137002935, 137683826, 140927354, 141097708, 141509152, 141534846, 141534878, 141535724, 141698547, 142382351, 143622412, 144213605, 144388331, 144529633, 145776750, 145909569, 154200849, 171709553, 171829540, 171829545, 172892516, 172934749, 175052532, 175052874, 175953385, 176343461, 176345027, 176346735, 186604357, 186661260, 191868623, 192331124, 192331126, 192331148, 193501469, 193532129 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
